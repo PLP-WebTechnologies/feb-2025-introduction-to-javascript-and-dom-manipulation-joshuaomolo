30bb276acf7e6200d9ee70cef4bc5515b4f3ba33
Joshua Omolo <69705493+joshuaomolo@users.noreply.github.com>
2025-03-19 13:24:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-let tl = getElementById("");
+let about = document.getElementsByClassName("about");
+let tl = document.getElementById("title");
+about.style.color = "orange";
+tl.style.fontSize = "30px";
